16c609129d4c1c438f24e239b0440c7716dcf6ec
David Dalton <daviddalton@Davids-MacBook-Pro.local>
2024-12-30 19:22:30 -0500

    add nav item

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -30,9 +30,15 @@ function NavBar() {
         navigate('/still-in')
     }
 
+    function goToInMeeting() {
+        navigate('/in-meeting')
+    }
+
     function buildTitle(): string {
         if (location.pathname.includes("/still-in")) {
             return "Whose Still In Anyways";
+        } else if (location.pathname.includes("/in-meeting")) {
+            return "Who's David Talking To"
         } else {
             return "Whose Turn Is It Anyways"
         }
@@ -44,6 +50,8 @@ function NavBar() {
                 <h1 className={"title application-text"} onClick={goHome}>{buildTitle()}</h1>
                 <Button onClick={goToTurnIsIt} sx={{color: 'navy', fontFamily: 'Ubuntu'}}>Turn Is It</Button>
                 <Button onClick={goToStillIn} sx={{color: 'navy', fontFamily: 'Ubuntu'}}>Still In</Button>
+                <Button onClick={goToInMeeting} sx={{color: 'navy', fontFamily: 'Ubuntu'}}>In Meeting</Button>
+
                 { user ? <div><h1 className={"user-name-text"}>{user?.displayName}</h1><h2 onClick={attemptSignOut}>Sign Out</h2></div> : <Button sx={{color: 'navy', fontFamily: 'Ubuntu'}} onClick={signIn}>Sign In</Button> }
             </Container>
         </div>
